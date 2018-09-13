ab4dc9d66fe1d8fc556318eeac8f4f2617323b77
Oleg <dydykin.oleg2013@gmail.com>
2018-09-13 18:08:15 +0300

    Sign in working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    root_url
+  end
+
+  # def after_sign_out_path_for(esource_ore_scope)
+  #   reqest.referrer
+  # end
 end
---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,9 @@
+class CustomersController < ApplicationController
+  before_action :authenticate_user!
+
+  def main
+  end
+
+  def profile
+  end
+end
