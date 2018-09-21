413bd6480c38a1567b4439e08f80fc854d7d8141
Hainar <dydykin.oleg2013@gmail.com>
2018-09-21 12:14:10 +0300

    renamed: customers -> orders

--- app/controllers/customers_controller.rb
+++
@@ -1,9 +0,0 @@
-class CustomersController < ApplicationController
-  before_action :authenticate_user!
-
-  def main
-  end
-
-  def profile
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'customers/main', as: 'user_root'
-  get 'customers/profile'
+  get 'orders/index', as: 'user_root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'customers#main'
+  root to: 'orders#index'
 end
